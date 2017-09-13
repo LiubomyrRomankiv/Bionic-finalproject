75ee450834b1d7613c47c0b536dfadc8b9fd7e7d
Liubomyr <liubomyrromankiv@gmail.com>
2017-09-13 17:21:57 +0300

    Admin page modified

--- src/helpers.js
+++
@@ -1,5 +0,0 @@
-import Handlebars from 'handlebars';
-
-Handlebars.registerHelper('answerType', function(type) {
-  return type;
-});
--- src/pages/admin/admin.page.js
+++ src/pages/admin/admin.page.js
@@ -1,6 +1,13 @@
 'use strict';
 
 import _ from 'lodash';
+import handlebars from 'handlebars';
+
+import tests from '../../components/test/tests.json';
+
+import testForm from './templates/newquestionForm.template.html';
+import textAnswer from './templates/checkAnswer.template.html';
+import checkAnswer from './templates/textAnswer.template.html';
 
 import Page from '../page';
 import adminPageContent from './admin.page.html';
@@ -9,14 +16,31 @@ class AdminPage extends Page {
   constructor(url){
     super(url);
     this.content = adminPageContent;
+    this.data = { tests };
     this.userStatus = {
       guest: false,
       user: false,
       admin: true
     };
   }
+
   whenPageRendered() {
-    
+    this.renderQuestionForm();
+  }
+
+  renderQuestionForm() {
+    let formContainer = document.querySelector('#question-form_block');
+    let html = handlebars.compile(testForm)();
+    formContainer.innerHTML = html;
+  }
+
+  renderAnswers(type) {
+    let formContainer = document.querySelector('.answers-block');
+    let html = handlebars.compile(checkAnswer)();
+    if( type === 'text' ){
+      html = handlebars.compile(textAnswer)();
+    }
+    formContainer.innerHTML = html;
   }
 }
 
